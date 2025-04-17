9df1740a22956648bf4c08bd237317241e154235
Wflow998 <workflow01998@gmail.com>
2025-04-17 17:08:09 +0000

    New Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Redirect to the welcome page
   redirect("/welcome");
+  return null; // This line is never reached but satisfies the component requirement
 }
